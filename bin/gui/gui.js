db89e24b3b750faed7b987cb7c6a6b9b7303499e
sasodoma <saso.doma@gmail.com>
2020-08-20 15:35:43 +0200

    GUI

--- bin/gui/gui.js
+++ bin/gui/gui.js
@@ -52,3 +52,11 @@ app.get('/revokeClient', function(req, res) {
 		res.send("Specify Username!");
 	}
 });
+
+//	Getting data from OpenVPN server
+app.get('/getOvpn', function(req, res) {
+	username = req.query.username;
+	res.setHeader('Content-Type', 'application/x-openvpn-profile');
+	res.setHeader('Content-Disposition', 'attachment; filename="' + username +'.ovpn"');
+	exec('/usr/local/bin/ovpn_getclient ' + username, function(error, stdout, stderr){ res.end(stdout); });
+});
